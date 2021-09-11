3e62deb0dddaa2cea391080be9a847613152f17c
Gray007 <cmzlelicanin@gmail.com>
2021-09-11 19:19:45 +0200

    the phonebook - 3.16

--- index.js
+++ index.js
@@ -34,7 +34,6 @@ app.get('/api/persons/:id', (request, response) => {
 app.post('/api/persons', (request, response) => {
   const body = request.body
   
-  
   if (!body.content === undefined) {
     return response.status(400).json({
       error: 'content missing'
@@ -52,16 +51,31 @@ app.post('/api/persons', (request, response) => {
   })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndRemove(request.params.id)
-    .then(person => {
-      if (person) {
-        response.status(204).end()
-      } else {
-        response.status(400).end()
-      }
+    .then(result => {
+      response.status(204).end()
     })
+    .catch(error => next(error))
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
